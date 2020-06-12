dde449ae0997178012e8f309a1393b181bce9a54
Alkiek <kalkiek@MGC000010441>
2020-06-12 14:29:31 -0400

    Initial commit. Cookie cutter generated structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze the political affiliations of Reddit users using their flairs, and subreddits through their networks',
+    author='Kenan Alkiek / University of Michigan / Blablablab',
+    license='MIT',
+)
