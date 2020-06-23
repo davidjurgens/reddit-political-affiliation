2e70af2bad16605edc3415fc24f24d7605486f31
Alkiek <kalkiek@MGC000010441>
2020-06-23 19:03:36 -0400

    Script to download flair data

---
+++ src/data/download_flair_data.py
@@ -0,0 +1,32 @@
+import sys
+import csv
+import json
+from collections import defaultdict
+
+"""  Script to parse out user flair information from Reddit comments and posts """
+
+assert len(sys.argv) == 3, '2 input arguments required. (1) path to the input file (2) output directory to save TSV'
+in_path, out_path = sys.argv[1], sys.argv[2]
+flair_count = 0
+user_flairs = defaultdict(lambda: defaultdict(set))
+
+with open(in_path) as f:
+    for count, line in enumerate(f):
+        submission = json.loads(f.readline())
+        username, flair, subreddit = submission['author'], submission['author_flair_text'], submission['subreddit']
+
+        if flair:
+            user_flairs[username][subreddit].add(flair)
+            flair_count += 1
+
+        if count % 1000000 == 0:
+            print("Completed %d lines for file %s" % (count, in_path))
+
+flair_percent = flair_count / float(count)
+print("Percentage of users with a flair {}".format(flair_percent))
+print("Number of unique users: {}".format(len(list(user_flairs.keys()))))
+
+with open(out_path, 'wt') as out_file:
+    tsv_writer = csv.writer(out_file, delimiter='\t')
+    for username, subreddit_flairs in user_flairs.items():
+        tsv_writer.writerow([username, dict(subreddit_flairs)])
