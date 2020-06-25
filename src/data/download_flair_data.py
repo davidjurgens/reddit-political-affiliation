e60fbc70f7cc952dc03f30a2f081dc713b3033ae
Alkiek <kalkiek@MGC000010441>
2020-06-24 20:06:50 -0400

    Account for bz2 and xz files. Add some print statements for debugging

--- src/data/download_flair_data.py
+++ src/data/download_flair_data.py
@@ -1,31 +1,42 @@
 import sys
 import csv
 import bz2
+import lzma
 import json
 from collections import defaultdict
 
 """  Script to parse out user flair information from Reddit comments and posts """
 
 assert len(sys.argv) == 3, '2 input arguments required. (1) path to the input file (2) output directory to save TSV'
+
 in_file, out_path = sys.argv[1], sys.argv[2]
+print("Starting parse of file {}".format(in_file))
 flair_count = 0
 user_flairs = defaultdict(lambda: defaultdict(set))
 
-with bz2.open(in_file) as f:
-    for count, line in enumerate(f):
-        submission = json.loads(f.readline())
-        username, flair, subreddit = submission['author'], submission['author_flair_text'], submission['subreddit']
+if in_file[-3:] == "bz2":
+    f = bz2.open(in_file)
+elif in_file[-2:] == "xz":
+    f = lzma.open(in_file)
+else:
+    raise AssertionError("Invalid extension for " + in_file + ". Expecting bz2 or xz file")
 
-        if flair:
-            user_flairs[username][subreddit].add(flair)
-            flair_count += 1
+for count, line in enumerate(f):
+    submission = json.loads(f.readline())
+    username, flair, subreddit = submission['author'], submission['author_flair_text'], submission['subreddit']
 
-        if count % 1000000 == 0:
-            print("Completed %d lines for file %s" % (count, in_file))
+    if flair:
+        user_flairs[username][subreddit].add(flair)
+        flair_count += 1
+
+    if count % 1000000 == 0:
+        print("Completed %d lines for file %s" % (count, in_file))
+
+f.close()
 
 flair_percent = flair_count / float(count)
-print("Percentage of users with a flair {}".format(flair_percent))
-print("Number of unique users: {}".format(len(list(user_flairs.keys()))))
+print("Percentage of users with a flair {} for file {}".format(flair_percent, in_file))
+print("Number of unique users for file {} is {}".format(in_file, len(list(user_flairs.keys()))))
 
 with open(out_path + in_file, 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
