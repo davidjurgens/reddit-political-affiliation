e7ea3b32617f23abe67354437881ff962dac572c
kalkiek <kenan.r.alkiek@gmail.com>
2020-07-01 14:12:20 -0400

    Fix filename print. TSV use list not set

--- src/data/download_flair_data.py
+++ src/data/download_flair_data.py
@@ -20,13 +20,13 @@ def get_file_handle(file_path):
     raise AssertionError("Invalid extension for " + file_path + ". Expecting bz2 or xz file")
 
 
-def parse_submissions(file_handle):
+def parse_submissions(file_name, file_pointer):
     """ Return a users subreddits with their associated flair(s) """
 
     flair_count = 0
     user_flairs = defaultdict(lambda: defaultdict(set))
 
-    for count, line in enumerate(file_handle):
+    for count, line in enumerate(file_pointer):
         try:
             submission = json.loads(f.readline())
             username, flair, subreddit = submission['author'], submission['author_flair_text'], submission['subreddit']
@@ -39,7 +39,7 @@ def parse_submissions(file_handle):
             print("Failed to parse line: {}".format(line))
 
         if count % 1000000 == 0 and count > 0:
-            print("Completed %d lines for file %s" % (count, file_handle))
+            print("Completed %d lines for file %s" % (count, file_name))
 
     flair_percent = flair_count / float(count)
     return user_flairs, flair_percent
@@ -49,7 +49,7 @@ def output_to_tsv(out_file, user_flairs):
     with open(out_file, 'wt') as out_file:
         tsv_writer = csv.writer(out_file, delimiter='\t')
         for username, subreddit_flairs in user_flairs.items():
-            tsv_writer.writerow([username, dict(subreddit_flairs)])
+            tsv_writer.writerow([username, list(subreddit_flairs)])
 
 
 if __name__ == '__main__':
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 
     print("Starting parse of file {}".format(in_file_path))
     f = get_file_handle(in_file_path)
-    flair_data, flair_percent = parse_submissions(f)
+    flair_data, flair_percent = parse_submissions(in_file_path, f)
     f.close()
 
     print("Percentage of users with a flair {} for file {}".format(flair_percent, in_file_path))
