4f884b6bb23fac501b199050ae3250a7297c1afe
kalkiek <kenan.r.alkiek@gmail.com>
2020-07-18 21:47:14 -0400

    Accept arguments to run on any year

--- src/models/generate_null_ppr.py
+++ src/models/generate_null_ppr.py
@@ -1,15 +1,18 @@
+import argparse
 import csv
 import glob
-import random
 import pickle
+import random
+from collections import Counter
 
 import networkx as nx
 from tqdm.notebook import tqdm
 
 
-def build_bipartite_graph(directory):
+def build_bipartite_graph(directory, min_sub_threshold=500):
     files = glob.glob(directory)
 
+    filtered_subreddits = get_subreddits_above_threshold(files, min_sub_threshold)
     g = nx.Graph()
 
     users = set()
@@ -29,19 +32,44 @@ def build_bipartite_graph(directory):
             users.add(user)
             subreddits.add(subreddit)
 
-            if not g.has_node(user):
-                g.add_node(user)
-            if not g.has_node(subreddit):
-                g.add_node(subreddit)
+            if subreddit in filtered_subreddits:
+                if not g.has_node(user):
+                    g.add_node(user)
+                if not g.has_node(subreddit):
+                    g.add_node(subreddit)
 
-            if g.has_edge(user, subreddit):
-                g[user][subreddit]['weight'] += freq
-            else:
-                g.add_edge(user, subreddit, weight=freq)
+                if g.has_edge(user, subreddit):
+                    g[user][subreddit]['weight'] += freq
+                else:
+                    g.add_edge(user, subreddit, weight=freq)
+
+    return filter_single_edge_nodes(g)
+
+
+def get_subreddits_above_threshold(files, min_sub_threshold):
+    """ Get subreddits that have a given number of submissions """
+
+    sub_frequencies = Counter()
+    for fname in tqdm(files, desc='Processing all files'):
+        with open(fname, 'rt') as f:
+            lines = f.readlines()
+
+        for line in tqdm(lines, position=1, desc='Build graph from file'):
+            user, subreddit, freq = line[:-1].split('\t')
+            subreddit = 'r/' + subreddit
+            sub_frequencies[subreddit] += int(freq)
+
+    return {sub: count for sub, count in sub_frequencies.items() if count >= min_sub_threshold}
+
+
+def filter_single_edge_nodes(g):
+    """ Get rid of users who only have an edge to one subreddit """
+    to_remove = [n for (n, deg) in g.degree() if deg == 1]
+    g.remove_nodes_from(to_remove)
     return g
 
 
-def get_representation(g, sample):
+def get_personalization(g, sample):
     n = g.number_of_nodes()
     personalization = {}
 
@@ -57,7 +85,7 @@ def compute_ppr_random_sample(g, sample_size, repeat=3):
     for i in range(repeat):
         print("Computing pagerank with random seed of size: {}".format(sample_size))
         sample = random.sample(g.nodes(), sample_size)
-        ppr = nx.pagerank(g, personalization=get_representation(g, sample))
+        ppr = nx.pagerank(g, personalization=get_personalization(g, sample))
         yield ppr
 
 
@@ -79,15 +107,23 @@ def load_existing_graph(input_path):
 
 if __name__ == '__main__':
 
-    sample_size = 4000
-    # network_dir = '/shared/0/projects/reddit-political-affiliation/data/bipartite-networks/2015*.tsv'
-    # g = build_bipartite_graph(network_dir)
-    g = load_existing_graph('/shared/0/projects/reddit-political-affiliation/data/bipartite-networks/2018_graph.pickle')
+    parser = argparse.ArgumentParser(description='Generate null PPR models for normalization')
+    parser.add_argument('seed_size', type=int)
+    parser.add_argument('year', type=str)
+    parser.add_argument('--repeat', type=int, default=5)
+    parser.add_argument('--g', help='Path to existing graph', type=str)
+    args = parser.parse_args()
+
+    if args.g:
+        g = load_existing_graph(args.g)
+    else:
+        directory = '/shared/0/projects/reddit-political-affiliation/data/bipartite-networks/' + args.year + '*.tsv'
+
     out_directory = '/shared/0/projects/reddit-political-affiliation/data/ppr-scores/'
 
-    for result in compute_ppr_random_sample(g, sample_size, repeat=15):
+    for result in compute_ppr_random_sample(g, args.seed_size, repeat=args.repeat):
         # Generate a random 'id' for the file. Just need a way to save under different names
         fid = random.randrange(1, 10 ** 6)
-        fname = out_directory + "2018_" + str(fid) + '.tsv'
+        fname = out_directory + args.year + "_" + str(fid) + '.tsv'
         print("{} is complete".format(fname))
         output_to_tsv(fname, result)
