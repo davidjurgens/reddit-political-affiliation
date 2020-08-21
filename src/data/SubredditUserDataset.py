5c6af5f81427030a3b7ac23ab23dd4aa9c30d4b6
Kenan Alkiek <kalkiek@ford.com>
2020-08-21 17:38:37 -0400

    Build subreddit user dataset

---
+++ src/data/SubredditUserDataset.py
@@ -0,0 +1,66 @@
+import random
+
+import numpy as np
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+
+class SubredditUserDataset(Dataset):
+
+    def __init__(self, user_subreddits, all_subreddits, user_to_politics, num_negative_samples=5, max_users=-1):
+        self.pos_and_neg_samples = []
+        # Mappings to the embedding dimensions
+        self.user_to_idx = {}
+        self.subreddit_to_idx = {}
+
+        def get_sub_idx(subreddit):
+            if subreddit in self.subreddit_to_idx:
+                sub_idx = self.subreddit_to_idx[subreddit]
+            else:
+                sub_idx = len(self.subreddit_to_idx)
+                self.subreddit_to_idx[subreddit] = len(self.subreddit_to_idx)
+            return sub_idx
+
+        num_users = len(user_subreddits) if max_users < 0 else max_users
+
+        for i, (user, subreddits) in enumerate(tqdm(user_subreddits.items(), total=num_users)):
+            if i >= num_users:
+                break
+
+            if user in user_to_politics:
+                politics = user_to_politics[user]
+            else:
+                politics = -1
+
+            self.user_to_idx[user] = len(self.user_to_idx)
+            user_idx = self.user_to_idx[user]
+
+            # Add all the positive samples
+            for subreddit in subreddits:
+                sub_idx = get_sub_idx(subreddit)
+                self.pos_and_neg_samples.append((np.array([user_idx, sub_idx]), politics, 1))
+
+            # Choose fixed negative samples
+            neg = []
+            num_neg = len(subreddits) * num_negative_samples
+            # guard against super active users?
+            num_neg = min(num_neg, len(all_subreddits) - num_neg)
+            while len(neg) < num_neg:
+                sub = all_subreddits[random.randint(0, len(all_subreddits) - 1)]
+                if sub not in subreddits:  # Check if also in neg?
+                    neg.append(sub)
+            for _ in neg:
+                sub_idx = get_sub_idx(subreddit)
+                self.pos_and_neg_samples.append((np.array([user_idx, sub_idx]), politics, 0))
+
+    def num_users(self):
+        return len(self.user_to_idx)
+
+    def num_subreddits(self):
+        return len(self.subreddit_to_idx)
+
+    def __len__(self):
+        return len(self.pos_and_neg_samples)
+
+    def __getitem__(self, idx):
+        return self.pos_and_neg_samples[idx]
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,82 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import glob
+from collections import defaultdict, Counter
+
+import torch
+from torch.utils.data import random_split
+from tqdm import tqdm
+
+from src.data.SubredditUserDataset import SubredditUserDataset
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def build_dataset(network_directory, flair_directory, validation_split=0.1, months=-1):
+    network_files = glob.glob(network_directory)[:months]
+    user_subreddits, vocab, all_subreddits = build_user_to_subreddits(network_files)
+    flair_files = glob.glob(flair_directory)
+    user_to_politics = read_political_affiliations(flair_files)
+    training_dataset = SubredditUserDataset(user_subreddits, all_subreddits, user_to_politics)
+
+    validation_size = int(validation_split * len(training_dataset))
+    train_size = len(training_dataset) - validation_size
+
+    # Fix the seed size for reproducibility
+    torch.manual_seed(42)
+    training, validation = random_split(training_dataset, [train_size, validation_size])
+    print("Train size: {} Validation size: {}".format(train_size, validation_size))
+    return training_dataset, training, validation, vocab
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+def build_user_to_subreddits(files):
+    vocab = set()
+    user_subreddits = defaultdict(set)
+    all_subreddits = set()
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    for fname in tqdm(files, desc="Processing all files"):
+        print(fname)
+        with open(fname, 'rt') as f:
+            lines = f.readlines()
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+        for line in tqdm(lines, position=1, desc='Building vocab from file'):
+            user, subreddit, freq = line[:-1].split('\t')
+            vocab.add(user)
+            vocab.add(subreddit)
+            user_subreddits[user].add(subreddit)
+            all_subreddits.add(subreddit)
 
-    main()
+    all_subreddits = list(all_subreddits)
+    print("Length of vocab: " + str(len(vocab)))
+    print("User count: " + str(len(user_subreddits)))
+    print("Subreddit count: " + str(len(all_subreddits)))
+
+    return user_subreddits, vocab, all_subreddits
+
+
+def read_political_affiliations(files):
+    user_to_politic_counts = defaultdict(Counter)
+
+    for fname in tqdm(files):
+        with open(fname, 'rt') as f:
+            for line in f:
+                user, politics, freq = line.split('\t')
+                user_to_politic_counts[user][politics] += int(freq)
+
+    print("User to politic counts: " + str(len(user_to_politic_counts)))
+    print(list(user_to_politic_counts.items())[:10])
+
+    user_to_politics = {}
+    for u, pc in user_to_politic_counts.items():
+        if len(pc) > 1:
+            continue
+        user_to_politics[u] = list(pc.keys())[0]
+
+    print('Saw political affiliations for %d users' % len(user_to_politics))
+    return convert_affiliations_to_binary(user_to_politics)
+
+
+def convert_affiliations_to_binary(user_to_politics):
+    for user, politics in user_to_politics.items():
+        if politics == "Democrat":
+            user_to_politics[user] = 0
+        else:
+            user_to_politics[user] = 1
+
+    return user_to_politics
