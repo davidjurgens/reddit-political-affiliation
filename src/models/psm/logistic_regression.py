391f58f98a9f75bd053b7dff96df1851c7e5e366
kalkiek <kenan.r.alkiek@gmail.com>
2021-02-12 15:23:08 -0500

    Setup logistic regression to run on any number of months

--- src/models/psm/logistic_regression.py
+++ src/models/psm/logistic_regression.py
@@ -1,26 +1,35 @@
+import glob
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 
-# SETTINGS
-features_file = '/shared/0/projects/reddit-political-affiliation/data/psm/features/RC_2019-09.tsv'
-test_split = 0.1
+TEST_SPLIT = 0.1
 
-print("Reading in existing features")
-df = pd.read_csv(features_file, sep='\t')
-train, test = train_test_split(df, test_size=test_split)
-print("Total number of users in train: {}. Total number of users in test: {}".format(len(train), len(test)))
 
-print("Training logistic regression model")
-print(train.columns)
-y = train['is_political']
-X = train.drop('is_political', axis=1)
-clf = LogisticRegression(random_state=0).fit(X, y)
+def read_in_features_df(in_files):
+    print("Total # of months of data: {}".format(len(in_files)))
+    return pd.concat((pd.read_csv(f, sep='\t') for f in in_files))
 
-y_labels, X_test = test['is_political'], test.drop('is_political', axis=1)
-y_preds = clf.predict(X_test)
 
-print(classification_report(y_labels, y_preds))
-print(accuracy_score(y_labels, y_preds))
+def train_logisitic_clf(df_train):
+    print("Training logistic regression model")
+    y, X = df_train['is_political'], df_train.drop('is_political', axis=1)
+    return LogisticRegression(random_state=0).fit(X, y)
+
+
+def test_logistic_clf(clf, df_test):
+    y_labels, X_test = df_test['is_political'], df_test.drop('is_political', axis=1)
+    y_preds = clf.predict(X_test)
+    print(classification_report(y_labels, y_preds))
+    print(accuracy_score(y_labels, y_preds))
+
+
+if __name__ == '__main__':
+    files = glob.glob('/shared/0/projects/reddit-political-affiliation/data/psm/features/silver/*.tsv')
+    df = read_in_features_df(files)
+    train, test = train_test_split(df, test_size=TEST_SPLIT)
+    print("Total number of users in train: {}. Total number of users in test: {}".format(len(train), len(test)))
+    clf = train_logisitic_clf(train)
+    test_logistic_clf(clf, test)
