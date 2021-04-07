f4afd3deffc9173e48131eb1ef6c1f0d7916c385
kalkiek <kenan.r.alkiek@gmail.com>
2021-04-07 11:42:15 -0400

    Move some code from notebooks into scripts and run on each data source

---
+++ src/models/usernameclassifier/lstm_usesrname_classifier.py
@@ -0,0 +1,202 @@
+import sys
+import torch
+from torch import nn
+import torch.optim as optim
+from torchtext import data
+from collections import defaultdict
+import pandas as pd
+from torchtext.legacy.data import Field, LabelField, TabularDataset, BucketIterator
+from collections import defaultdict, Counter
+from tqdm import tqdm
+
+sys.path.append('/home/kalkiek/projects/reddit-political-affiliation/')
+
+from src.models.usernameclassifier.UsernameClassifier import UsernameClassifier
+
+# SETTINGS
+BASE_DIR = '/shared/0/projects/reddit-political-affiliation/data/username-labels/'
+
+print("Building datasets for train, dev, and test")
+TEXT = Field(tokenize=list, batch_first=True, include_lengths=True)
+LABEL = LabelField(dtype=torch.float, batch_first=True)
+fields = [('text', TEXT), ('label', LABEL), (None, None), ]
+
+training_data = TabularDataset(BASE_DIR + 'user2label.silver.train.csv', format='csv', fields=fields,
+                               skip_header=True)
+dev_data = TabularDataset(BASE_DIR + 'user2label.silver.dev.csv', format='csv', fields=fields, skip_header=True)
+test_data = TabularDataset(BASE_DIR + 'user2label.silver.test.csv', format='csv', fields=fields, skip_header=True)
+
+print("Sample of preprocessed text: {}".format(vars(training_data.examples[0])))
+
+print("Initializing glove embeddings")
+TEXT.build_vocab(training_data, min_freq=1)
+LABEL.build_vocab(training_data)
+
+# No. of unique tokens in text
+print("Size of TEXT vocabulary:", len(TEXT.vocab))
+
+# No. of unique tokens in label
+print("Size of LABEL vocabulary:", len(LABEL.vocab))
+
+# Commonly used words
+print("Commonly used words")
+print(TEXT.vocab.freqs.most_common(10))
+
+# Word dictionary
+print(TEXT.vocab.stoi)
+
+# check whether cuda is available
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+
+# set batch size
+BATCH_SIZE = 128
+
+# Load an iterator
+train_iterator, valid_iterator = BucketIterator.splits(
+    (training_data, dev_data),
+    batch_size=BATCH_SIZE,
+    sort_key=lambda x: len(x.text),
+    sort_within_batch=True,
+    device=device)
+
+size_of_vocab = len(TEXT.vocab)
+embedding_dim = 15
+num_hidden_nodes = 256
+num_output_nodes = 1
+num_layers = 2
+bidirection = True
+dropout = 0.2
+
+# instantiate the model
+model = UsernameClassifier(size_of_vocab, embedding_dim, num_hidden_nodes, num_output_nodes, num_layers,
+                           bidirectional=True, dropout=dropout)
+
+# define optimizer and loss
+optimizer = optim.Adam(model.parameters())
+criterion = nn.BCELoss()
+
+
+# define metric
+def binary_accuracy(preds, y):
+    # round predictions to the closest integer
+    rounded_preds = torch.round(preds)
+
+    correct = (rounded_preds == y).float()
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+# push to cuda if available
+model = model.to(device)
+criterion = criterion.to(device)
+
+
+def train(model, iterator, optimizer, criterion):
+    # initialize every epoch
+    epoch_loss = 0
+    epoch_acc = 0
+
+    # set the model in training phase
+    model.train()
+
+    for batch in tqdm(iterator):
+        # resets the gradients after every batch
+        optimizer.zero_grad()
+
+        # retrieve text and no. of words
+        text, text_lengths = batch.text
+
+        # convert to 1D tensor
+        predictions = model(text, text_lengths).squeeze()
+
+        # compute the loss
+        loss = criterion(predictions, batch.label)
+
+        # compute the binary accuracy
+        acc = binary_accuracy(predictions, batch.label)
+
+        # backpropage the loss and compute the gradients
+        loss.backward()
+
+        # update the weights
+        optimizer.step()
+
+        # loss and accuracy
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+    # initialize every epoch
+    epoch_loss = 0
+    epoch_acc = 0
+
+    # deactivating dropout layers
+    model.eval()
+
+    # deactivates autograd
+    with torch.no_grad():
+        for batch in iterator:
+            # retrieve text and no. of words
+            text, text_lengths = batch.text
+
+            # convert to 1d tensor
+            predictions = model(text, text_lengths).squeeze()
+
+            # compute loss and accuracy
+            loss = criterion(predictions, batch.label)
+            acc = binary_accuracy(predictions, batch.label)
+
+            # keep track of loss and accuracy
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+N_EPOCHS = 10
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+
+    # train the model
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+
+    # evaluate the model
+    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+
+    # save the best model
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'saved_weights.pt')
+
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
+
+# Check out some of the predictions
+model.eval()
+
+dev_pred_df = defaultdict(list)
+
+# deactivates autograd
+with torch.no_grad():
+    for batch in tqdm(valid_iterator):
+
+        # retrieve text and no. of words
+        text, text_lengths = batch.text
+
+        # convert to 1d tensor
+        predictions = model(text, text_lengths).squeeze()
+
+        for i, cids in enumerate(text.cpu()):
+            username = ''.join([TEXT.vocab.itos[cid] for cid in cids])
+            pred = predictions[i].cpu().item()
+            dev_pred_df['username'].append(username)
+            dev_pred_df['prediction'].append(pred)
+
+        # print(text)
+        # break
+dev_pred_df = pd.DataFrame(dev_pred_df)
+dev_pred_df.head()
---
+++ src/models/usernameclassifier/prepare_username_data.py
@@ -0,0 +1,93 @@
+import sys
+
+sys.path.append('/home/kalkiek/projects/reddit-political-affiliation/')
+
+from src.features.political_affiliations.conglomerate_affiliations import *
+
+# Settings
+train_path = '/shared/0/projects/reddit-political-affiliation/data/conglomerate-affiliations/train.tsv'
+dev_path = '/shared/0/projects/reddit-political-affiliation/data/conglomerate-affiliations/dev.tsv'
+test_path = '/shared/0/projects/reddit-political-affiliation/data/conglomerate-affiliations/test.tsv'
+
+print("Reading in conglomerate political affiliations")
+train_df = pd.read_csv(train_path, index_col=False, delimiter='\t')
+dev_df = pd.read_csv(dev_path, index_col=False, delimiter='\t')
+test_df = pd.read_csv(test_path, index_col=False, delimiter='\t')
+
+print("Splitting into flair, gold, and silver datasets")
+flair_train = train_df[train_df['source'] == 'flair']
+gold_train = train_df[train_df['source'] == 'gold']
+silver_train = train_df[train_df['source'] == 'silver']
+
+print("Flair train length: {}".format(len(flair_train)))
+print("Gold train length: {}".format(len(gold_train)))
+print("Silver train length: {}".format(len(silver_train)))
+
+flair_dev = dev_df[dev_df['source'] == 'flair']
+gold_dev = dev_df[dev_df['source'] == 'gold']
+silver_dev = dev_df[dev_df['source'] == 'silver']
+
+print("Flair dev length: {}".format(len(flair_dev)))
+print("Gold dev length: {}".format(len(gold_dev)))
+print("Silver dev length: {}".format(len(silver_dev)))
+
+flair_test = test_df[test_df['source'] == 'flair']
+gold_test = test_df[test_df['source'] == 'gold']
+silver_test = test_df[test_df['source'] == 'silver']
+
+print("Flair test length: {}".format(len(flair_test)))
+print("Gold test length: {}".format(len(gold_test)))
+print("Silver test length: {}".format(len(silver_test)))
+
+print("Dropping everything except username and politics")
+flair_train = flair_train[['username', 'politics']]
+gold_train = gold_train[['username', 'politics']]
+silver_train = silver_train[['username', 'politics']]
+
+flair_dev = flair_dev[['username', 'politics']]
+gold_dev = gold_dev[['username', 'politics']]
+silver_dev = silver_dev[['username', 'politics']]
+
+flair_test = flair_test[['username', 'politics']]
+gold_test = gold_test[['username', 'politics']]
+silver_test = silver_test[['username', 'politics']]
+
+
+def get_binary_political_labels(political_labels):
+    binary_labels = []
+    for label in political_labels:
+        if label == 'Democrat':
+            binary_labels.append(0)
+        else:
+            binary_labels.append(1)
+    return binary_labels
+
+
+print("Adding binary label for politics. Democrat = 0 and Republican = 1")
+flair_train['binary_label'] = get_binary_political_labels(flair_train['politics'])
+gold_train['binary_label'] = get_binary_political_labels(gold_train['politics'])
+silver_train['binary_label'] = get_binary_political_labels(silver_train['politics'])
+
+flair_dev['binary_label'] = get_binary_political_labels(flair_dev['politics'])
+gold_dev['binary_label'] = get_binary_political_labels(gold_dev['politics'])
+silver_dev['binary_label'] = get_binary_political_labels(silver_dev['politics'])
+
+flair_test['binary_label'] = get_binary_political_labels(flair_test['politics'])
+gold_test['binary_label'] = get_binary_political_labels(gold_test['politics'])
+silver_test['binary_label'] = get_binary_political_labels(silver_test['politics'])
+
+print(flair_train.head())
+
+print("Outputting files to TSV")
+out_dir = '/shared/0/projects/reddit-political-affiliation/data/username-labels/'
+flair_train.to_csv(out_dir + 'user2label.flair.train.csv', index=False)
+gold_train.to_csv(out_dir + 'user2label.gold.train.csv', index=False)
+silver_train.to_csv(out_dir + 'user2label.silver.train.csv', index=False)
+
+flair_dev.to_csv(out_dir + 'user2label.flair.dev.csv', index=False)
+gold_dev.to_csv(out_dir + 'user2label.gold.dev.csv', index=False)
+silver_dev.to_csv(out_dir + 'user2label.silver.dev.csv', index=False)
+
+flair_test.to_csv(out_dir + 'user2label.flair.test.csv', index=False)
+gold_test.to_csv(out_dir + 'user2label.gold.test.csv', index=False)
+silver_test.to_csv(out_dir + 'user2label.silver.test.csv', index=False)
