143342c02e92a063071d56b2a5599d4b77c78ba8
Kenan Alkiek <kenan.r.alkiek@gmail.com>
2021-04-12 10:35:19 -0400

    Save collected usernames to TSV

--- src/features/collect_samples.py
+++ src/features/collect_samples.py
@@ -8,6 +8,7 @@ from src.features.political_affiliations.conglomerate_affiliations import get_al
 
 
 def collect_submissions(raw_files, users, count):
+    """ Grab posts or comments for a subset of users """
     # Sample evenly across the files
     samples_per_file = int(count / len(raw_files))
     samples = []
@@ -19,13 +20,13 @@ def collect_submissions(raw_files, users, count):
 
 
 def get_samples_from_file(raw_file, users, count):
+    assert type(users) is dict or type(users) is set  # Otherwise 'in' operation will take forever
     print("Collecting {} from {}".format(count, raw_file))
     collected = []
     for submission in read_submissions(raw_file):
-        user = submission['author']
+        user = submission.username
         if user in users:
-            subreddit, created = submission['subreddit'], submission['created_utc']
-            text = get_submission_text(submission)
+            subreddit, created, text = submission.subreddit, submission.created, submission.text
             entry = {'username': user, 'subreddit': subreddit, 'created': created, 'text': text}
             collected.append(entry)
 
@@ -39,6 +40,7 @@ def get_samples_from_file(raw_file, users, count):
 
 
 def collect_non_political_usernames(raw_files, political_users, count):
+    assert type(political_users) is dict or type(political_users) is set
     usernames_per_file = int(count / len(raw_files))
     usernames = set()
 
@@ -50,11 +52,12 @@ def collect_non_political_usernames(raw_files, political_users, count):
 
 
 def sample_non_political_users(month_file, political_users, count):
+    assert type(political_users) is dict or type(political_users) is set
     users = set()
     print("Sampling {} non political users from: {}".format(count, month_file))
 
     for submission in read_submissions(month_file):
-        username = submission['author']
+        username = submission.username
 
         if username not in political_users:
             users.add(username)
@@ -75,10 +78,26 @@ def output_samples_to_tsv(samples, out_tsv):
             f.write("{}\t{}\t{}\t{}\n".format(s['username'], s['subreddit'], s['created'], s['text']))
 
 
+def usernames_to_tsv(users, out_tsv):
+    print("Saving {} users to {}".format(len(users), out_tsv))
+    with open(out_tsv, 'w') as f:
+        for user in users:
+            f.write("{}\n".format(user))
+
+
+def read_usernames_from_tsv(in_tsv):
+    users = set()
+    with open(in_tsv, 'r') as f:
+        for line in f.readlines():
+            users.add(line.strip())
+
+    return users
+
+
 def read_samples_from_tsv(in_tsv):
     samples = []
     with open(in_tsv, 'r') as f:
-        for line in f:
+        for line in f.readlines():
             try:
                 username, subreddit, created, text = line.split('\t')
                 entry = {'username': username, 'subreddit': subreddit, 'created': created, 'text': text}
@@ -88,31 +107,23 @@ def read_samples_from_tsv(in_tsv):
     return samples
 
 
-def get_submission_text(sub):
-    text = ""
-    if "body" in sub:
-        text += sub['body'].lower()
-    if "title" in sub:
-        text += " " + sub['title'].lower()
-    return " ".join(text.split())
-
-
 if __name__ == '__main__':
     files = glob('/shared/2/datasets/reddit-dump-all/RC/*.zst')
     files.extend(glob('/shared/2/datasets/reddit-dump-all/RC/*.xz'))
     files.extend(glob('/shared/2/datasets/reddit-dump-all/RC/*.bz2'))
 
     output_directory = '/shared/0/projects/reddit-political-affiliation/data/sample-submissions/'
+    political_users = get_all_political_users()
+    non_political_users = collect_non_political_usernames(files, political_users, count=100000)
+    usernames_to_tsv(non_political_users, output_directory + 'non_political_usernames.tsv')
+
+    print("Collecting submissions of non political users")
+    non_political_user_samples = collect_submissions(files, non_political_users, count=100000)
+    output_samples_to_tsv(non_political_user_samples, output_directory + 'non_political_samples.tsv')
 
     # Grab sample submissions for political users
-    political_users = get_all_political_users()
     print("Total number of political users: {}".format(len(political_users)))
     print("Grabbing submissions for political users")
     political_user_samples = collect_submissions(files, political_users, count=100000)
     output_samples_to_tsv(political_user_samples, output_directory + 'political_samples.tsv')
 
-    print("Grabbing non political users")
-    non_political_users = collect_non_political_usernames(files, political_users, count=9999999)
-    print("Collecting submissions of non political users")
-    non_political_user_samples = collect_submissions(files, non_political_users, count=100000)
-    output_samples_to_tsv(non_political_user_samples, output_directory + 'non_political_samples.tsv')
