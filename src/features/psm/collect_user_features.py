6f46f717ce610e52d705a8e9ccfba5e14786c1ee
kalkiek <kenan.r.alkiek@gmail.com>
2021-04-21 07:47:03 -0400

    Move PSM features into Module. Update script to collect all political/non-political/bad actor features in one run instead of 3

--- src/features/psm/collect_user_features.py
+++ src/features/psm/collect_user_features.py
@@ -11,24 +11,36 @@ from src.data.date_helper import read_submissions, get_all_raw_files
 from src.features.political_affiliations.conglomerate_affiliations import get_all_political_users
 from src.features.bad_actors.bad_actors import read_in_bad_actors_from_tsv
 from src.features.collect_samples import read_usernames_from_tsv
-from src.models.psm.features import top_subreddits, political_subreddits
+from src.features.psm.features import top_subreddits, political_subreddits
 
 
-def collect_user_submission_data(month_file, users, user_features):
+""" Script to collect behavioral user features like number of posts or total karma """
+
+
+def collect_user_submission_data(month_file, pol_users, non_pol_users, bad_actors, pol_user_features,
+                                 non_pol_user_features, bad_actor_features):
     print("Collecting user features for month: {}".format(month_file))
 
     for submission in read_submissions(month_file):
-        if submission.username in users:
-            submission_type = 'post' if submission.is_post() else 'comment'
-            top_subreddit = 1 if submission.subreddit in top_subreddits else 0
-            pol_subreddit = 1 if submission.subreddit in political_subreddits else 0
+        if submission.username in pol_users:
+            pol_user_features[submission.username].append(get_submission_metadata(submission))
+        elif submission.username in non_pol_users:
+            non_pol_user_features[submission.username].append(get_submission_metadata(submission))
+        elif submission.username in bad_actors:
+            bad_actor_features[submission.username].append(get_submission_metadata(submission))
 
-            entry = {'username': submission.username, 'subreddit': submission.subreddit, 'score': submission.score,
-                     'submission_type': submission_type, 'gilded': submission.gilded, 'created': submission.created,
-                     'total_awards': submission.total_awards, 'controversiality': submission.controversiality,
-                     'top_subreddit': top_subreddit, 'political_subreddit': pol_subreddit}
-            user_features[submission.username].append(entry)
-    return user_features
+    return pol_user_features, non_pol_user_features, bad_actor_features
+
+
+def get_submission_metadata(submission):
+    submission_type = 'post' if submission.is_post() else 'comment'
+    top_subreddit = 1 if submission.subreddit in top_subreddits else 0
+    pol_subreddit = 1 if submission.subreddit in political_subreddits else 0
+
+    return {'username': submission.username, 'subreddit': submission.subreddit, 'score': submission.score,
+            'submission_type': submission_type, 'gilded': submission.gilded, 'created': submission.created,
+            'total_awards': submission.total_awards, 'controversiality': submission.controversiality,
+            'top_subreddit': top_subreddit, 'political_subreddit': pol_subreddit}
 
 
 def build_user_features_df(user_features):
@@ -91,15 +103,20 @@ def get_time_of_day(created_utc):
     return 'night'
 
 
-def run_collect(users, files, out_tsv):
-    print(
-        "Collecting user features for {} users over {} files and saving to {}".format(len(users), len(files), out_tsv))
-    user_features = defaultdict(list)
+def run_collect(pol_users, non_pol_users, bad_actors, files, out_dir):
+    pol_user_features, non_pol_user_features, bad_actor_features = defaultdict(list), defaultdict(list), defaultdict(
+        list)
     for m_file in files:
-        user_features = collect_user_submission_data(m_file, users, user_features)
-        df = build_user_features_df(user_features)
-        print("Saving features dataframe to TSV")
-        df.to_csv(out_tsv, sep='\t')
+        pol_user_features, non_pol_user_features, bad_actor_features = \
+            collect_user_submission_data(m_file, pol_users, non_pol_users, bad_actors, pol_user_features,
+                                         non_pol_user_features, bad_actor_features)
+        pol_df = build_user_features_df(pol_user_features)
+        non_pol_df = build_user_features_df(non_pol_user_features)
+        bad_actors_df = build_user_features_df(bad_actor_features)
+        print("Saving feature dataframes to TSV")
+        pol_df.to_csv(out_dir + 'all_political.tsv', sep='\t', index=False)
+        non_pol_df.to_csv(out_dir + 'all_non_political.tsv', sep='\t', index=False)
+        bad_actors_df.to_csv(out_dir + 'bad_actors.tsv', sep='\t', index=False)
 
 
 def read_in_non_pol_user_features():
@@ -120,20 +137,15 @@ def read_in_bad_actor_features():
 if __name__ == '__main__':
     files = get_all_raw_files()
 
-    # Repeat for bad actors
+    political_users = get_all_political_users()
+    print("Total number of political users: {}".format(len(political_users)))
+
     bad_actors = read_in_bad_actors_from_tsv(
         ['/shared/0/projects/reddit-political-affiliation/data/bad-actors/bad_actors_90_days_1_flip_flop.tsv'])
-    run_collect(bad_actors, files,
-                out_tsv='/shared/0/projects/reddit-political-affiliation/data/user-features/bad_actors.tsv')
 
     non_political_users = read_usernames_from_tsv(
         '/shared/0/projects/reddit-political-affiliation/data/sample-submissions/non_political_usernames.tsv')
     print("Total of {} non political users".format(len(non_political_users)))
-    run_collect(non_political_users, files,
-                out_tsv='/shared/0/projects/reddit-political-affiliation/data/user-features/all_non_political.tsv')
 
-    users = get_all_political_users()
-    print("Total number of users: {}".format(len(users)))
-    run_collect(users, files,
-                out_tsv='/shared/0/projects/reddit-political-affiliation/data/user-features/all_political.tsv')
-
+    run_collect(political_users, non_political_users, bad_actors, files,
+                out_dir='/shared/0/projects/reddit-political-affiliation/data/user-features/')
