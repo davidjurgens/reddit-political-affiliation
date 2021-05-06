5c860c00cff053c9f7a04c99366c0b807b00a5aa
XMUBQ <1156483159@qq.com>
2021-05-06 09:59:52 -0400

    fix some merge

--- src/features/interactions/log_reg_interaction.py
+++ src/features/interactions/log_reg_interaction.py
@@ -1,27 +1,48 @@
 import sys
-
+import glob
+import numpy as np
 import pandas as pd
+import random
+from collections import *
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 sys.path.append('/home/kalkiek/projects/reddit-political-affiliation/')
-
-from src.data.data_helper import grab_bot_accounts
 from src.features.interactions.political_comment import PoliticalComment
-from transformers import BertTokenizer, BertForSequenceClassification, BertConfig
+from transformers import BertTokenizer,BertForSequenceClassification, BertConfig, AdamW
 import torch
+import torch.optim as optim
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 
-if sys.version_info[0] < 3:
-    pass
-else:
-    pass
 
+if sys.version_info[0] < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+def grab_bot_accounts():
+    fname = '/shared/0/projects/prosocial/known-bots.tsv'
+    bots = []
+
+    with open(fname, 'rt') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            bots.append(line.split('\t')[1])
+
+    print("Known bots: %d" % len(bots))
+    return bots
 
 def read_in_comments(in_file, count=-1):
     comments = []
     with open(in_file, 'r', encoding="utf-8") as f:
-        for line in tqdm(f, total=count if count > 0 else 137629803):
+        for line in tqdm(f,total=count if count>0 else 137629803):
             line = line.strip()
             try:
                 comment_id, parent_id, username, subreddit, created, politics, text = line.split('\t')
@@ -37,12 +58,11 @@ def read_in_comments(in_file, count=-1):
     print("Total number of political comments: {}".format(len(comments)))
     return comments
 
-
 class Intents(Dataset):
     def __init__(self, dataframe):
         self.len = len(dataframe)
         self.data = dataframe
-        # self.Label_Map={'Democrat':0,'Republican':1}
+        #self.Label_Map={'Democrat':0,'Republican':1}
 
     def __getitem__(self, index):
         utterance = self.data.text[index]
@@ -53,20 +73,16 @@ class Intents(Dataset):
     def __len__(self):
         return self.len
 
-
 def get_interactions(from_party, to_party):
     from_comment_ids = set(df_comments[df_comments['politics'] == from_party]['comment_id'].tolist())
     to_comment_ids = set(df_comments[df_comments['politics'] == to_party]['comment_id'].tolist())
-    interactions = df_comments[
-        (df_comments['comment_id'].isin(from_comment_ids) & df_comments['parent_id'].isin(to_comment_ids))]
-    dyad = [from_party + 'to' + to_party] * len(interactions)
-    interactions['FromPolitics'] = from_party
-    interactions['ToPolitics'] = to_party
+    interactions = df_comments[(df_comments['comment_id'].isin(from_comment_ids) & df_comments['parent_id'].isin(to_comment_ids))]
+    dyad=[from_party+'to'+to_party]*len(interactions)
+    interactions['FromPolitics']=from_party
+    interactions['ToPolitics']=to_party
     return interactions
 
-
-def prepare_transformer_features(seq_1, max_seq_length=64, zero_pad=True, include_CLS_token=True,
-                                 include_SEP_token=True):
+def prepare_transformer_features(seq_1, max_seq_length=64, zero_pad=True, include_CLS_token=True, include_SEP_token=True):
     # Tokenize Input
     tokens = tokenizer.tokenize(seq_1)[0:max_seq_length - 2]
     # Initialize Tokens
@@ -89,19 +105,20 @@ def prepare_transformer_features(seq_1, max_seq_length=64, zero_pad=True, includ
     return torch.tensor(input_ids), torch.tensor(input_mask)
 
 
-def prepare_toxicity_in_batch(sent, mask):
-    Y = []
+def prepare_toxicity_in_batch(sent,mask):
+    Y=[]
     mask = mask.cuda()
     sent = sent.cuda()
     output = model.forward(sent, attention_mask=mask)[0]
     soft_output = F.softmax(output, dim=1)
-    # print(soft_output.shape)
-    toxicity_score = soft_output[:, 1]
+   # print(soft_output.shape)
+    toxicity_score=soft_output[:,1]
     Y.extend(list(toxicity_score.cpu().detach().numpy()))
     # print(soft_output,soft_output[0][1].item())
-    # Y.append(soft_output[0][1].item())
+    #Y.append(soft_output[0][1].item())
 
     return Y
+
 
 
 if __name__ == '__main__':
@@ -117,7 +134,7 @@ if __name__ == '__main__':
     bots = grab_bot_accounts()
     bots.extend('[deleted]')
     in_file = '/shared/0/projects/reddit-political-affiliation/data/interactions/all_comments_filtered.tsv'
-    comments = read_in_comments(in_file, count=-1)
+    comments = read_in_comments(in_file,count=-1)
     df_comments = pd.DataFrame(comments)
     # print(df_comments.head(10))
     top_sub = 499
@@ -161,22 +178,22 @@ if __name__ == '__main__':
                'RepublicantoDemocrat': 3, 'DemocrattoUnknown': 4, 'RepublicantoUnknown': 5,
                'UnknowntoDemocrat': 6, 'UnknowntoRepublican': 7}
 
-    all_data = pd.concat(comment_lists, ignore_index=True)
+    all_data = pd.concat(comment_lists,ignore_index=True)
     all_data = all_data.sample(frac=1).reset_index()
     print(all_data.head())
-    all_set = Intents(all_data)
+    all_set=Intents(all_data)
     for i in range(5):
         print(all_set[i][2])
     params = {'batch_size': 64, 'shuffle': False, 'drop_last': False, 'num_workers': 1}
     all_loader = DataLoader(all_set, **params)
-    iter_length = len(all_loader)
-    Y = []
+    iter_length=len(all_loader)
+    Y=[]
     for i, (sent, mask, label) in tqdm(enumerate(all_loader), total=iter_length):
-        # print(sent.shape,mask.shape)
-        Y.extend(prepare_toxicity_in_batch(sent, mask))
-    print(len(Y), len(all_set))
+        #print(sent.shape,mask.shape)
+        Y.extend(prepare_toxicity_in_batch(sent,mask))
+    print(len(Y),len(all_set))
 
-    all_data['toxicity'] = Y
+    all_data['toxicity']=Y
     saved_path = '/shared/0/projects/reddit-political-affiliation/data/interactions_features/real_interactions_feature.tsv'
     all_data.to_csv(saved_path, sep='\t')
 
@@ -198,8 +215,4 @@ if __name__ == '__main__':
     # df_result = pd.read_csv(TESTDATA, sep=',')
     # pd.set_option('display.max_rows', None)
     # #print(df_result.sort_values(by=['   coef   ']).head(50))
-<<<<<<< HEAD
     # print(df_result.sort_values(by=['   coef   '],ascending=False).head(50))
-=======
-    # print(df_result.sort_values(by=['   coef   '],ascending=False).head(50))
->>>>>>> e658e0f0cd20ed1b06f8d827aebfc29e5841541d
---
+++ src/models/textclassifier/random_baseline.py
@@ -0,0 +1,42 @@
+import sys
+
+sys.path.append('/home/kalkiek/projects/reddit-political-affiliation/')
+from src.features.interactions.political_comment import PoliticalComment
+import json
+from sklearn.metrics import classification_report
+from sklearn.utils import resample
+from json import JSONDecodeError
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+
+# cong_dir = '/shared/0/projects/reddit-political-affiliation/data/conglomerate-affiliations/'
+# train_cong = pd.read_csv(cong_dir + 'train.tsv', sep='\t')
+# dev_cong = pd.read_csv(cong_dir + 'dev.tsv', sep='\t')
+# test_cong = pd.read_csv(cong_dir + 'test.tsv', sep='\t')
+#
+# train_user = set(train_cong['username'])
+# dev_user = set(dev_cong['username'])
+# test_user = set(test_cong['username'])
+#
+# sorted_train_cong=train_cong.sort_values(["username","source"])
+# distinct_train_cong=sorted_train_cong.drop_duplicates(subset="username",keep="first").sample(frac=1)
+# sorted_test_cong = test_cong.sort_values(["username", "source"])
+# distinct_test_cong = sorted_test_cong.drop_duplicates(subset="username", keep="first").sample(frac=1)
+# sorted_dev_cong = dev_cong.sort_values(["username", "source"])
+# distinct_dev_cong = sorted_dev_cong.drop_duplicates(subset="username", keep="first").sample(frac=1)
+#
+# print(distinct_train_cong.shape,distinct_test_cong.shape,distinct_dev_cong.shape)
+
+saved_path = '/shared/0/projects/reddit-political-affiliation/data/bert-text-classify/users_prediction/'
+evaluating = 'flair'  # can be flair gold silver all
+eval_from_flair = pd.read_csv(saved_path + "Roberta_flair_downsampling_" + evaluating + ".tsv", sep='\t')
+eval_from_flair = eval_from_flair.drop_duplicates(subset="username", keep="last").reset_index()
+print(eval_from_flair)
+label=eval_from_flair['politics']
+#rand=np.random.randint(2, size=len(label))
+rand=1-np.zeros(len(label))
+crf = classification_report(label, rand, output_dict=True)
+crf2 = classification_report(label, rand, output_dict=False)
+print(crf)
+print(crf2)
