53340bf87948fc7d390f0aa3d994465ae8739392
unknown <kenan.r.alkiek@gmail.com>
2021-05-10 00:57:58 -0400

    Add in community labelled users into existing train/dev/test split

--- src/features/political_affiliations/conglomerate_affiliations.py
+++ src/features/political_affiliations/conglomerate_affiliations.py
@@ -2,6 +2,7 @@ import glob
 import random
 import sys
 from collections import defaultdict
+from itertools import islice
 
 import pandas as pd
 
@@ -11,11 +12,14 @@ sys.path.append('/home/kalkiek/projects/reddit-political-affiliation/')
 
 from src.features.political_affiliations.comment_political_affiliations import read_in_user_politics
 from src.features.political_affiliations.flair_political_affiliations import read_in_flair_affiliations
+from src.features.political_affiliations.community_labels import get_user_politics_for_community_labels
 
 ''' 
     Read in all of the political affiliations (silver, gold, flair) and create train dev and test sets with the joined
     data. Hardcoded directory paths because they are static 
 '''
+
+OUTPUT_DIRECTORY = "/shared/0/projects/reddit-political-affiliation/data/conglomerate-affiliations/"
 
 
 def grab_all_data_sources():
@@ -31,14 +35,14 @@ def grab_all_data_sources():
     gold_data = read_in_user_politics(gold_files)
     flair_data = read_in_flair_affiliations(flair_files)
 
-    return silver_data, gold_data, flair_data
+    return {'silver': silver_data, 'gold': gold_data, 'flair': flair_data}
 
 
-def build_df(silver, gold, flair):
+def build_df(data_by_source):
     user_entries = defaultdict(list)
 
     print("Working on silver data")
-    for user, user_politics in silver.items():
+    for user, user_politics in data_by_source['silver'].items():
         for entry in user_politics:
             row = {'username': user, 'source': 'silver', 'politics': entry['politics'], 'subreddit': entry['subreddit'],
                    'created': entry['created']}
@@ -46,7 +50,7 @@ def build_df(silver, gold, flair):
                 user_entries[user].append(row)
 
     print("Working on gold data")
-    for user, user_politics in gold.items():
+    for user, user_politics in data_by_source['gold'].items():
         for entry in user_politics:
             row = {'username': user, 'source': 'gold', 'politics': entry['politics'], 'subreddit': entry['subreddit'],
                    'created': entry['created']}
@@ -54,7 +58,7 @@ def build_df(silver, gold, flair):
                 user_entries[user].append(row)
 
     print("Working on flair data")
-    for user, flair_data in flair.items():
+    for user, flair_data in data_by_source['flair'].items():
         for entry in flair_data:
             row = {'username': user, 'source': 'flair', 'politics': entry['politics'], 'subreddit': entry['subreddit'],
                    'created': entry['created']}
@@ -76,6 +80,7 @@ def build_df(silver, gold, flair):
 def shuffle_dict_keys(user_entries):
     print("Shuffling usernames")
     users = list(user_entries.keys())
+    random.seed(42)
     random.shuffle(users)
     shuffled_dict = {}
     for user in users:
@@ -150,16 +155,52 @@ def get_dev_political_affiliations():
 
 
 def get_all_political_users():
-    print("Grabbing all political users")
-    silver_data, gold_data, flair_data = grab_all_data_sources()
-    df = build_df(silver_data, gold_data, flair_data)
-    return set(df['username'].tolist())
+    # print("Grabbing all political users")
+    # df = build_df(grab_all_data_sources())
+    # return set(df['username'].tolist())
+    pass
+
+
+def add_in_community_labels():
+    """ Adding in community politics without changing the existing train/dev/test users """
+    # Grab the community political affiliations and split them randomly 80/10/10
+    community_politics = get_user_politics_for_community_labels()
+
+    community_politics = shuffle_dict_keys(community_politics)
+    train_length = int(len(community_politics) * 0.8)
+    dev_length, test_length = int(len(community_politics) * .1), int(len(community_politics) * .1)
+
+    train_users = dict(islice(community_politics.items(), train_length))
+    dev_users = dict(islice(community_politics.items(), train_length + 1, train_length + dev_length))
+    test_users = dict(
+        islice(community_politics.items(), train_length + dev_length + 1, train_length + dev_length + test_length))
+
+    print(len(train_users), len(dev_users), len(test_users))
+
+    # Read in train/dev/test
+    train_df = pd.read_csv(OUTPUT_DIRECTORY + 'train.tsv', sep='\t', index_col=False)
+    dev_df = pd.read_csv(OUTPUT_DIRECTORY + 'dev.tsv', sep='\t', index_col=False)
+    test_df = pd.read_csv(OUTPUT_DIRECTORY + 'test.tsv', sep='\t', index_col=False)
+
+    add_users_into_existing_df_and_save(train_df, train_users, 'train.tsv')
+    add_users_into_existing_df_and_save(dev_df, dev_users, 'dev.tsv')
+    add_users_into_existing_df_and_save(test_df, test_users, 'test.tsv')
+
+
+def add_users_into_existing_df_and_save(df, user_politics, fname):
+    rows = []
+    for user, politics in user_politics.items():
+        entry = {'username': user, 'politics': politics, 'source': 'community', 'subreddit': '', 'created': ''}
+        rows.append(entry)
+
+    new_df = pd.DataFrame(rows)
+    result = pd.concat([df, new_df], ignore_index=True)
+    print(len(df), len(result), fname)
+    result.to_csv(OUTPUT_DIRECTORY + fname, sep='\t', index=False)
 
 
 if __name__ == '__main__':
-    out_directory = "/shared/0/projects/reddit-political-affiliation/data/conglomerate-affiliations/"
-    silver, gold, flair = grab_all_data_sources()
-    df = build_df(silver, gold, flair)
+    df = build_df(grab_all_data_sources())
     print(df.head(10))
     train, dev, test = split_into_train_dev_test(df)
 
@@ -169,6 +210,6 @@ if __name__ == '__main__':
 
     validate_no_overlap(train, dev, test)
 
-    train.to_csv(out_directory + "train.tsv", sep='\t')
-    dev.to_csv(out_directory + "dev.tsv", sep='\t')
-    test.to_csv(out_directory + "test.tsv", sep='\t')
+    train.to_csv(OUTPUT_DIRECTORY + "train.tsv", sep='\t')
+    dev.to_csv(OUTPUT_DIRECTORY + "dev.tsv", sep='\t')
+    test.to_csv(OUTPUT_DIRECTORY + "test.tsv", sep='\t')
