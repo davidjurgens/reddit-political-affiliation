a72c6953b2ce1c67e94b26a398436fc33be7b52a
unknown <kenan.r.alkiek@gmail.com>
2021-05-10 01:51:45 -0400

    Add community politics

--- src/models/usernameclassifier/prepare_username_data.py
+++ src/models/usernameclassifier/prepare_username_data.py
@@ -18,39 +18,48 @@ print("Splitting into flair, gold, and silver datasets")
 flair_train = train_df[train_df['source'] == 'flair']
 gold_train = train_df[train_df['source'] == 'gold']
 silver_train = train_df[train_df['source'] == 'silver']
+community_train = train_df[train_df['source'] == 'community']
 
 print("Flair train length: {}".format(len(flair_train)))
 print("Gold train length: {}".format(len(gold_train)))
 print("Silver train length: {}".format(len(silver_train)))
+print("Community train length: {}".format(len(community_train)))
 
 flair_dev = dev_df[dev_df['source'] == 'flair']
 gold_dev = dev_df[dev_df['source'] == 'gold']
 silver_dev = dev_df[dev_df['source'] == 'silver']
+community_dev = dev_df[dev_df['source'] == 'community']
 
 print("Flair dev length: {}".format(len(flair_dev)))
 print("Gold dev length: {}".format(len(gold_dev)))
 print("Silver dev length: {}".format(len(silver_dev)))
+print("Community dev length: {}".format(len(community_dev)))
 
 flair_test = test_df[test_df['source'] == 'flair']
 gold_test = test_df[test_df['source'] == 'gold']
 silver_test = test_df[test_df['source'] == 'silver']
+community_test = test_df[test_df['source'] == 'community']
 
 print("Flair test length: {}".format(len(flair_test)))
 print("Gold test length: {}".format(len(gold_test)))
 print("Silver test length: {}".format(len(silver_test)))
+print("Community test length: {}".format(len(community_test)))
 
 print("Dropping everything except username and politics")
 flair_train = flair_train[['username', 'politics']]
 gold_train = gold_train[['username', 'politics']]
 silver_train = silver_train[['username', 'politics']]
+community_train = community_train[['username', 'politics']]
 
 flair_dev = flair_dev[['username', 'politics']]
 gold_dev = gold_dev[['username', 'politics']]
 silver_dev = silver_dev[['username', 'politics']]
+community_dev = community_dev[['username', 'politics']]
 
 flair_test = flair_test[['username', 'politics']]
 gold_test = gold_test[['username', 'politics']]
 silver_test = silver_test[['username', 'politics']]
+community_test = community_test[['username', 'politics']]
 
 
 def get_binary_political_labels(political_labels):
@@ -67,14 +76,17 @@ print("Adding binary label for politics. Democrat = 0 and Republican = 1")
 flair_train['binary_label'] = get_binary_political_labels(flair_train['politics'])
 gold_train['binary_label'] = get_binary_political_labels(gold_train['politics'])
 silver_train['binary_label'] = get_binary_political_labels(silver_train['politics'])
+community_train['binary_label'] = get_binary_political_labels(community_train['politics'])
 
 flair_dev['binary_label'] = get_binary_political_labels(flair_dev['politics'])
 gold_dev['binary_label'] = get_binary_political_labels(gold_dev['politics'])
 silver_dev['binary_label'] = get_binary_political_labels(silver_dev['politics'])
+community_dev['binary_label'] = get_binary_political_labels(community_dev['politics'])
 
 flair_test['binary_label'] = get_binary_political_labels(flair_test['politics'])
 gold_test['binary_label'] = get_binary_political_labels(gold_test['politics'])
 silver_test['binary_label'] = get_binary_political_labels(silver_test['politics'])
+community_test['binary_label'] = get_binary_political_labels(community_test['politics'])
 
 print(flair_train.head())
 
@@ -83,11 +95,14 @@ out_dir = '/shared/0/projects/reddit-political-affiliation/data/username-labels/
 flair_train.to_csv(out_dir + 'user2label.flair.train.csv', index=False)
 gold_train.to_csv(out_dir + 'user2label.gold.train.csv', index=False)
 silver_train.to_csv(out_dir + 'user2label.silver.train.csv', index=False)
+silver_train.to_csv(out_dir + 'user2label.community.train.csv', index=False)
 
 flair_dev.to_csv(out_dir + 'user2label.flair.dev.csv', index=False)
 gold_dev.to_csv(out_dir + 'user2label.gold.dev.csv', index=False)
 silver_dev.to_csv(out_dir + 'user2label.silver.dev.csv', index=False)
+silver_dev.to_csv(out_dir + 'user2label.community.dev.csv', index=False)
 
 flair_test.to_csv(out_dir + 'user2label.flair.test.csv', index=False)
 gold_test.to_csv(out_dir + 'user2label.gold.test.csv', index=False)
 silver_test.to_csv(out_dir + 'user2label.silver.test.csv', index=False)
+silver_test.to_csv(out_dir + 'user2label.community.test.csv', index=False)
