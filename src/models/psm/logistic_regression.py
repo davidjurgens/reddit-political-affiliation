4ed23ff0851e820922eecb2e2fea4916c3be4d0e
XMUBQ <1156483159@qq.com>
2021-05-18 01:09:32 -0400

    Update logistic_regression.py

--- src/models/psm/logistic_regression.py
+++ src/models/psm/logistic_regression.py
@@ -6,26 +6,32 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+import pickle
 
 sys.path.append('/home/kalkiek/projects/reddit-political-affiliation/')
 
 TEST_SPLIT = 0.1
-
+random_state=42
+drop_source=False
 
 def train_combined_model(df_train):
     df_train = set_is_either_flip(df_train)
-    train, test = train_test_split(df_train, test_size=0.1)
+    train, test = train_test_split(df_train, test_size=0.1,random_state=random_state)
     train, test = train.drop(['dem_flip', 'rep_flip'], axis=1), test.drop(['dem_flip', 'rep_flip'], axis=1)
 
     y, X = train['is_flip'], train.drop('is_flip', axis=1)
-    clf = LogisticRegression(random_state=0).fit(X, y)
+    clf = LogisticRegression(random_state=random_state).fit(X, y)
 
     y_labels, X_test = test['is_flip'], test.drop('is_flip', axis=1)
     y_preds = clf.predict(X_test)
 
     print("RESULTS FOR COMBINED MODEL")
     print(classification_report(y_labels, y_preds))
+    print(classification_report(y_labels, y_preds,output_dict=True))
     print(accuracy_score(y_labels, y_preds))
+
+    filename = ('drop_source' if drop_source else '')+'combined_model.sav'
+    pickle.dump(clf, open(filename, 'wb'))
 
     # TODO: Save predictions
 
@@ -34,24 +40,28 @@ def train_combined_model(df_train):
         coef_dict[feat] = coef
 
     coef_dict = {k: v for k, v in sorted(coef_dict.items(), key=lambda item: item[1])}
-    print(coef_dict)
+    #print(coef_dict)
 
     return clf
 
 
 def train_rep_to_dem_model(df_train):
-    train, test = train_test_split(df_train, test_size=0.1)
+    train, test = train_test_split(df_train, test_size=0.1,random_state=random_state)
     train, test = train.drop('dem_flip', axis=1), test.drop('dem_flip', axis=1)
 
     y, X = train['rep_flip'], train.drop('rep_flip', axis=1)
-    clf = LogisticRegression(random_state=0).fit(X, y)
+    clf = LogisticRegression(random_state=random_state).fit(X, y)
 
     y_labels, X_test = test['rep_flip'], test.drop('rep_flip', axis=1)
     y_preds = clf.predict(X_test)
 
     print("RESULTS FOR REP TO DEM MODEL")
     print(classification_report(y_labels, y_preds))
+    print(classification_report(y_labels, y_preds, output_dict=True))
     print(accuracy_score(y_labels, y_preds))
+
+    filename = ('drop_source' if drop_source else '')+'rep2dem_model.sav'
+    pickle.dump(clf, open(filename, 'wb'))
 
     # TODO: Save predictions
 
@@ -60,26 +70,31 @@ def train_rep_to_dem_model(df_train):
         coef_dict[feat] = coef
 
     coef_dict = {k: v for k, v in sorted(coef_dict.items(), key=lambda item: item[1])}
-    print(coef_dict)
+    #print(coef_dict)
 
     return clf
 
 
 def train_dem_to_rep_model(df_train):
-    train, test = train_test_split(df_train, test_size=0.1)
+    train, test = train_test_split(df_train, test_size=0.1,random_state=random_state)
 
     train, test = train.drop('rep_flip', axis=1), test.drop('rep_flip', axis=1)
     print(len(train), len(test))
 
     y, X = train['dem_flip'], train.drop('dem_flip', axis=1)
-    clf = LogisticRegression(random_state=0).fit(X, y)
+    clf = LogisticRegression(random_state=random_state).fit(X, y)
 
     y_labels, X_test = test['dem_flip'], test.drop('dem_flip', axis=1)
     y_preds = clf.predict(X_test)
 
+
     print("RESULTS FOR DEM TO REP MODEL")
     print(classification_report(y_labels, y_preds))
+    print(classification_report(y_labels, y_preds, output_dict=True))
     print(accuracy_score(y_labels, y_preds))
+
+    filename = ('drop_source' if drop_source else '')+'dem2rep_model.sav'
+    pickle.dump(clf, open(filename, 'wb'))
 
     # TODO: Save predictions
 
@@ -88,7 +103,7 @@ def train_dem_to_rep_model(df_train):
         coef_dict[feat] = coef
 
     coef_dict = {k: v for k, v in sorted(coef_dict.items(), key=lambda item: item[1])}
-    print(coef_dict)
+    #print(coef_dict)
 
     return clf
 
@@ -123,6 +138,9 @@ if __name__ == '__main__':
     # Convert source from flair/community/gold into 0, 1, 2
     df['source'] = df['source'].astype('category').cat.codes
 
+    if drop_source:
+        df.drop(columns=['source'],inplace=True)
+    print (df.head())
     train_combined_model(df)
     train_rep_to_dem_model(df)
     train_dem_to_rep_model(df)
